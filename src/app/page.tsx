77dd56dafb59dbbb9c93763ceaaf0eacf310897a
Nalin Singh <nalinsingh20@gmail.com>
2024-04-26 01:57:17 +0530

    Update : Export Type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "~/components/atoms/button";
 
-export const Application = () => {
+const Application = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
       <Button>Invinio</Button>
     </main>
   );
 };
+
+export default Application;
