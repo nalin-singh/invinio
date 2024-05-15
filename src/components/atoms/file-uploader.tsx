72879902b8eeb8672a0967449415880fb72193d3
Nalin Singh <nalinsingh20@gmail.com>
2024-05-15 20:52:27 +0530

    Update : FileUploader

--- src/components/atoms/file-uploader.tsx
+++ src/components/atoms/file-uploader.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useDropzone } from "@uploadthing/react";
 import { useCallback, useState } from "react";
 import { toast } from "sonner";
@@ -6,6 +8,7 @@ import { generateClientDropzoneAccept } from "uploadthing/client";
 import { useUploadThing } from "~/utils/uploadthing";
 import { Button } from "./button";
 import { Input } from "./input";
+import { Upload } from "lucide-react";
 
 // Uses UploadThing API to Upload Files and returns a promise and the stored url
 const FileUploader = () => {
@@ -37,14 +40,25 @@ const FileUploader = () => {
   }, [files, startUpload]);
 
   return (
-    <div {...getRootProps()}>
+    <div
+      className="flex cursor-pointer flex-col  rounded-md border-2 border-dotted"
+      {...getRootProps()}
+    >
       <Input {...getInputProps()} />
-      <div>
-        {files.length > 0 && (
-          <Button onClick={uploadFile}>Upload {files.length} files</Button>
-        )}
-      </div>
-      Drop files here!
+      {files.length > 0 && (
+        <Button className="m-auto" onClick={uploadFile}>
+          Upload {files.length} files
+        </Button>
+      )}
+      {files.length === 0 && (
+        <div className="m-auto flex items-center gap-1">
+          <Upload className="h-4 w-4" />
+          <p className=" w-32 px-2 py-1 text-center text-xs text-muted-foreground">
+            Drop files here or <br />
+            Click to Upload
+          </p>
+        </div>
+      )}
     </div>
   );
 };
